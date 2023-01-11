3ada7fc149e781f6029c12744a912e405be06cb7
Devarth123 <devarth@tutanota.com>
2023-01-11 17:48:10 +0530

    hmm

--- pronic/Main.java
+++ pronic/Main.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 class Main{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt(),y=0,j=1,k=1;
-        while(true){            
-            ++j;
-            k+=2;
-            y = k * j;
-            if (x == y) {
-                System.out.println("Its a pronic no.");
-                break;
-            }
-            else if((y>x) || (y<x)){
-                System.out.println("Its not a pronic no.");
-                break;
+        System.out.println("Enter a number");
+        int x = in.nextInt();
+        for(int j=0;j<=((int)Math.sqrt(x)); ++j){
+            if ((j *(j+1)) == x){
+                System.out.println("Its a pronic number");
+                System.exit(0);
             }
         }
+        System.out.println("its not a pronic number");
     }
 }
